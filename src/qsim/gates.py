47a6d9973f7593cead18be011c0adf4fbe8da72c
Matthew (mattqsim) <matt.qsim@pm.me>
2026-02-03 20:40:41 +0000

    changes to gate

--- src/qsim/gates.py
+++ src/qsim/gates.py
@@ -1,5 +1,8 @@
 import numpy as np 
 
+
+## 1 qubit gates 
+
 I = np.array([
     [1, 0],
     [0, 1],
@@ -38,7 +41,9 @@ T = np.array([
 
 def RZ(theta: float) -> np.ndarray:
     """
-    RZ(theta) = exp(-i theta Z / 2) = [[e^{-iθ/2}, 0], [0, e^{+iθ/2}]]
+    RZ(theta) = exp(-i theta Z/2) = 
+    [[e^{-iθ/2}, 0], 
+     [0, e^{+iθ/2}]]
     """
     t = float(theta) / 2.0
     return np.array([
@@ -49,9 +54,9 @@ def RZ(theta: float) -> np.ndarray:
 
 def RY(theta: float) -> np.ndarray:
     """
-    RY(theta) = exp(-i theta Y / 2) =
-      [[cos(θ/2), -sin(θ/2)],
-       [sin(θ/2),  cos(θ/2)]]
+    RY(theta) = exp(-i theta Y/2) =
+    [[cos(θ/2), -sin(θ/2)],
+     [sin(θ/2),  cos(θ/2)]]
     """
     t = float(theta) / 2.0
     c = np.cos(t)
@@ -60,3 +65,84 @@ def RY(theta: float) -> np.ndarray:
         [c, -s],
         [s,  c],
     ], dtype=complex)
+
+
+def RX(theta: float) -> np.ndarray: 
+    """
+    RX(theta) = exp(-i theta X/2) =
+    [[cos(θ/2), -i sin(θ/2)],
+     [-i sin(θ/2), cos(θ/2)]]
+    """
+    t = float(theta) / 2.0
+    c = np.cos(t) 
+    s = np.sin(t) 
+    return np.array([
+        [c, -1j * s],
+        [-1j * s, c],
+    ], dtype=complex) 
+
+    
+## 2 qubit gates
+
+CNOT = np.array([
+    [1,0,0,0],
+    [0,1,0,0],
+    [0,0,0,1],
+    [0,0,1,0],
+], dtype=complex) 
+
+SWAP = np.array([
+    [1,0,0,0],
+    [0,0,1,0],
+    [0,1,0,0],
+    [0,0,0,1],
+], dtype=complex)
+
+CZ = np.array([
+    [1,0,0,0],
+    [0,1,0,0],
+    [0,0,1,0],
+    [0,0,0,-1],
+], dtype=complex)
+
+## CS is fixed controlled-phase, CP is float controlled phase
+
+CS = np.array([
+    [1,0,0,0],
+    [0,1,0,0],
+    [0,0,1,0],
+    [0,0,0,1j],
+], dtype=complex)
+
+def CP(theta: float) -> np.ndarray: 
+    theta = float(theta) 
+    return np.array([
+        [1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,0],
+        [0,0,0,np.exp(1j*theta)],
+    ], dtype=complex) 
+
+## 3 qubit gates 
+
+TOF = np.array([
+    [1,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0],
+    [0,0,0,1,0,0,0,0],
+    [0,0,0,0,1,0,0,0],
+    [0,0,0,0,0,1,0,0],
+    [0,0,0,0,0,0,0,1],
+    [0,0,0,0,0,0,1,0],
+], dtype=complex) 
+
+FRED = np.array([
+    [1,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0],
+    [0,0,0,1,0,0,0,0],
+    [0,0,0,0,1,0,0,0],
+    [0,0,0,0,0,0,1,0],
+    [0,0,0,0,0,1,0,0],
+    [0,0,0,0,0,0,0,1],
+], dtype=complex) 
